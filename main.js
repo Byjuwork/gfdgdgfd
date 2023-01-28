0eaca01c50097f6ca2699e90157b5437ffe435e4
Byjuwork <98567453+Byjuwork@users.noreply.github.com>
2023-01-27 16:23:29 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function setup() {
+	canvas =  createCanvas(600, 500);
+	canvas.center();
+
+	video = createCapture(VIDEO);
+	video.hide();
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
+function play()
+{
+	song.play();
+	song.setVolume(1);
+	song.rate(1);
+}
